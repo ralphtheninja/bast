1606722bc5812da0bd8329d81443fba3d8ff46ca
lms <ralphtheninja@riseup.net>
2014-04-02 22:55:14 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+var jsdom = require('jsdom')
+
+var url = process.argv[2]
+var selector = process.argv[3]
+
+var opts = {
+      url: url,
+      done: function (err, window) {
+        if (err) return console.log(err)
+        var addresses = []
+        var list = window.document.querySelectorAll(selector)
+        var length = list.length
+        for (var i = 0; i < length; ++i) {
+          var regex = /(^|\s+)(1[1-9A-HJ-NP-Za-km-z]{26,33})/g
+          var match = regex.exec(list[i].innerHTML)
+          if (match) {
+            addresses.push(match[2])
+          }
+        }
+        addresses.forEach(function (address, i) {
+          console.log(address)
+        })
+      }
+    }
+
+jsdom.env(opts)
