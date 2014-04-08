e80e24b6864a71188ec59809ac4769be4068492e
lms <ralphtheninja@riseup.net>
2014-04-08 22:16:29 +0200

    scrape function takes options object parameter

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 #!/usr/bin/env node
 var jsdom = require('jsdom')
 
-function scrape(url, selector, cb) {
-  var opts = {
-        url: url,
-        done: function (err, window) {
-          if (err) return cb(err)
-          var addresses = {}
-          var list = window.document.querySelectorAll(selector)
-          var length = list.length
-          for (var i = 0; i < length; ++i) {
-            var regex = /(^|\s+)(1[1-9A-HJ-NP-Za-km-z]{26,33})/g
-            var match = regex.exec(list[i].innerHTML)
-            if (match) {
-              var address = match[2]
-              addresses[address] = address
-            }
-          }
-          cb(null, Object.keys(addresses))
+function scrape(opts, cb) {
+  // TODO this could be done much better and needs to be more generic
+  var regex = opts.regex || /(^|\s+)(1[1-9A-HJ-NP-Za-km-z]{26,33})/g
+
+  jsdom.env({
+
+    url: opts.url,
+
+    done: function (err, window) {
+      if (err) return cb(err)
+      var addresses = {}
+      var list = window.document.querySelectorAll(opts.selector)
+      var length = list.length
+      for (var i = 0; i < length; ++i) {
+        var match = regex.exec(list[i].innerHTML)
+        if (match) {
+          var address = match[2]
+          if (typeof address == 'string') addresses[address] = address
         }
       }
-  jsdom.env(opts)
+      cb(null, Object.keys(addresses))
+    }
+
+  })
+
 }
 
 if (!module.parent) {
-  scrape(process.argv[2], process.argv[3], function (err, result) {
+  // TODO check for command line arguments
+  var opts = { url: process.argv[2], selector: process.argv[3] }
+  scrape(opts, function (err, result) {
     if (err) return process.stderr.write(err.toString())
     result.forEach(function (address) {
       process.stdout.write(address + '\n')
