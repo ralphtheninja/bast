c94735edf0e55b873b6a9dc61a645d721f0208d2
lms <ralphtheninja@riseup.net>
2014-04-08 20:09:36 +0200

    break out functionality into a function and export it

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 #!/usr/bin/env node
 var jsdom = require('jsdom')
 
-var url = process.argv[2]
-var selector = process.argv[3]
-
-var opts = {
-      url: url,
-      done: function (err, window) {
-        if (err) return console.log(err)
-        var addresses = {}
-        var list = window.document.querySelectorAll(selector)
-        var length = list.length
-        for (var i = 0; i < length; ++i) {
-          var regex = /(^|\s+)(1[1-9A-HJ-NP-Za-km-z]{26,33})/g
-          var match = regex.exec(list[i].innerHTML)
-          if (match) {
-            addresses[match[2]] = true
+function scrape(url, selector, cb) {
+  var opts = {
+        url: url,
+        done: function (err, window) {
+          if (err) return cb(err)
+          var addresses = {}
+          var list = window.document.querySelectorAll(selector)
+          var length = list.length
+          for (var i = 0; i < length; ++i) {
+            var regex = /(^|\s+)(1[1-9A-HJ-NP-Za-km-z]{26,33})/g
+            var match = regex.exec(list[i].innerHTML)
+            if (match) {
+              var address = match[2]
+              addresses[address] = address
+            }
           }
+          cb(null, Object.keys(addresses))
         }
-        Object.keys(addresses).forEach(function (address) {
-          console.log(address)
-        })
       }
-    }
+  jsdom.env(opts)
+}
 
-jsdom.env(opts)
+if (!module.parent) {
+  scrape(process.argv[2], process.argv[3], function (err, result) {
+    if (err) return process.stderr.write(err.toString())
+    result.forEach(function (address) {
+      process.stdout.write(address + '\n')
+    })
+  })
+}
+
+module.exports = scrape
+
